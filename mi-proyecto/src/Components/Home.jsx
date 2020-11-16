55a53e35ae6ee7fca1c598fdbf0870348a2176d8
aguriv <agustinrivero91@gmail.com>
2020-11-15 21:16:41 -0300

    home y cards

--- mi-proyecto/src/Components/Home.jsx
+++ mi-proyecto/src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../App.css";
 import Navigation from "./Navigation";
+import Carousel from "react-bootstrap/Carousel";
 import axios from "axios";
 import ProductCard from "./ProductCard";
 
@@ -21,10 +22,37 @@ const Home = () => {
   return (
     <>
       <Navigation />
-      <div className="container">
-        <h3 className="mt-4">PRODUCTOS</h3>
+      <div className="">
+        <Carousel>
+          <Carousel.Item>
+            <div className="d-block w-100 banner1"></div>
+            <Carousel.Caption className="bannerBox text-dark">
+              <h2>TU COCINA IDEAL</h2>
+              <h5>Para equiparla como más te guste.</h5>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="d-block w-100 banner2"></div>
 
-        <div className="row">
+            <Carousel.Caption className="bannerBox">
+              <h2>MIRA TODAS LAS TELES</h2>
+              <h5>O no las mires, pero compralas.</h5>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="d-block w-100 banner3"></div>
+
+            <Carousel.Caption className="bannerBox">
+              <h2>ENVIOS SIN CARGO A TODO EL PAIS</h2>
+              <h5>Y podés pagar con tu medio de pago favorito.</h5>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+      <div className="container">
+        <h3 className="mt-4">Destacados</h3>
+
+        <div className="row mt-4">
           {productsList &&
             productsList.map((product) => {
               return (
